a62a6a515a22059531b8bb47d982a586b8eb9565
DrGrid <seb.kung@gmail.com>
2015-04-06 17:27:19 +0200

    Cleaner Layout

--- build/ui_mainwindow.h
+++ build/ui_mainwindow.h
@@ -15,6 +15,7 @@
 #include <QtGui/QButtonGroup>
 #include <QtGui/QFormLayout>
 #include <QtGui/QGridLayout>
+#include <QtGui/QHBoxLayout>
 #include <QtGui/QHeaderView>
 #include <QtGui/QLabel>
 #include <QtGui/QLineEdit>
@@ -22,6 +23,7 @@
 #include <QtGui/QMenu>
 #include <QtGui/QMenuBar>
 #include <QtGui/QPushButton>
+#include <QtGui/QSpacerItem>
 #include <QtGui/QStatusBar>
 #include <QtGui/QToolBar>
 #include <QtGui/QWidget>
@@ -33,11 +35,8 @@ class Ui_MainWindow
 public:
     QAction *actionNew_Ticker;
     QWidget *centralWidget;
-    QWidget *layoutWidget;
-    QGridLayout *gridLayout;
-    QPushButton *setdown;
-    QPushButton *setpath;
-    QLineEdit *linepath;
+    QWidget *widget;
+    QHBoxLayout *horizontalLayout;
     QFormLayout *formLayout;
     QLabel *price;
     QLabel *label;
@@ -51,11 +50,16 @@ public:
     QLabel *label_5;
     QLabel *spread;
     QLabel *label_6;
-    QPushButton *setup;
-    QLineEdit *linedown;
+    QSpacerItem *horizontalSpacer;
+    QGridLayout *gridLayout;
     QLineEdit *lineup;
-    QLabel *label_down;
+    QPushButton *setup;
     QLabel *label_up;
+    QLineEdit *linedown;
+    QPushButton *setdown;
+    QLabel *label_down;
+    QLineEdit *linepath;
+    QPushButton *setpath;
     QMenuBar *menuBar;
     QMenu *menuFile;
     QToolBar *mainToolBar;
@@ -70,120 +74,130 @@ public:
         actionNew_Ticker->setObjectName(QString::fromUtf8("actionNew_Ticker"));
         centralWidget = new QWidget(MainWindow);
         centralWidget->setObjectName(QString::fromUtf8("centralWidget"));
-        layoutWidget = new QWidget(centralWidget);
-        layoutWidget->setObjectName(QString::fromUtf8("layoutWidget"));
-        layoutWidget->setGeometry(QRect(20, 10, 451, 108));
-        gridLayout = new QGridLayout(layoutWidget);
-        gridLayout->setSpacing(6);
-        gridLayout->setContentsMargins(11, 11, 11, 11);
-        gridLayout->setObjectName(QString::fromUtf8("gridLayout"));
-        gridLayout->setContentsMargins(0, 0, 0, 0);
-        setdown = new QPushButton(layoutWidget);
-        setdown->setObjectName(QString::fromUtf8("setdown"));
-
-        gridLayout->addWidget(setdown, 1, 2, 1, 1);
-
-        setpath = new QPushButton(layoutWidget);
-        setpath->setObjectName(QString::fromUtf8("setpath"));
-
-        gridLayout->addWidget(setpath, 2, 2, 1, 1);
-
-        linepath = new QLineEdit(layoutWidget);
-        linepath->setObjectName(QString::fromUtf8("linepath"));
-
-        gridLayout->addWidget(linepath, 2, 1, 1, 1);
-
+        widget = new QWidget(centralWidget);
+        widget->setObjectName(QString::fromUtf8("widget"));
+        widget->setGeometry(QRect(31, 11, 398, 108));
+        horizontalLayout = new QHBoxLayout(widget);
+        horizontalLayout->setSpacing(6);
+        horizontalLayout->setContentsMargins(11, 11, 11, 11);
+        horizontalLayout->setObjectName(QString::fromUtf8("horizontalLayout"));
+        horizontalLayout->setContentsMargins(0, 0, 0, 0);
         formLayout = new QFormLayout();
         formLayout->setSpacing(6);
         formLayout->setObjectName(QString::fromUtf8("formLayout"));
         formLayout->setFieldGrowthPolicy(QFormLayout::ExpandingFieldsGrow);
-        price = new QLabel(layoutWidget);
+        price = new QLabel(widget);
         price->setObjectName(QString::fromUtf8("price"));
 
         formLayout->setWidget(0, QFormLayout::LabelRole, price);
 
-        label = new QLabel(layoutWidget);
+        label = new QLabel(widget);
         label->setObjectName(QString::fromUtf8("label"));
 
         formLayout->setWidget(0, QFormLayout::FieldRole, label);
 
-        high = new QLabel(layoutWidget);
+        high = new QLabel(widget);
         high->setObjectName(QString::fromUtf8("high"));
 
         formLayout->setWidget(1, QFormLayout::LabelRole, high);
 
-        label_2 = new QLabel(layoutWidget);
+        label_2 = new QLabel(widget);
         label_2->setObjectName(QString::fromUtf8("label_2"));
 
         formLayout->setWidget(1, QFormLayout::FieldRole, label_2);
 
-        low = new QLabel(layoutWidget);
+        low = new QLabel(widget);
         low->setObjectName(QString::fromUtf8("low"));
 
         formLayout->setWidget(2, QFormLayout::LabelRole, low);
 
-        label_3 = new QLabel(layoutWidget);
+        label_3 = new QLabel(widget);
         label_3->setObjectName(QString::fromUtf8("label_3"));
 
         formLayout->setWidget(2, QFormLayout::FieldRole, label_3);
 
-        sell = new QLabel(layoutWidget);
+        sell = new QLabel(widget);
         sell->setObjectName(QString::fromUtf8("sell"));
 
         formLayout->setWidget(3, QFormLayout::LabelRole, sell);
 
-        label_4 = new QLabel(layoutWidget);
+        label_4 = new QLabel(widget);
         label_4->setObjectName(QString::fromUtf8("label_4"));
 
         formLayout->setWidget(3, QFormLayout::FieldRole, label_4);
 
-        bid = new QLabel(layoutWidget);
+        bid = new QLabel(widget);
         bid->setObjectName(QString::fromUtf8("bid"));
 
         formLayout->setWidget(4, QFormLayout::LabelRole, bid);
 
-        label_5 = new QLabel(layoutWidget);
+        label_5 = new QLabel(widget);
         label_5->setObjectName(QString::fromUtf8("label_5"));
 
         formLayout->setWidget(4, QFormLayout::FieldRole, label_5);
 
-        spread = new QLabel(layoutWidget);
+        spread = new QLabel(widget);
         spread->setObjectName(QString::fromUtf8("spread"));
 
         formLayout->setWidget(5, QFormLayout::LabelRole, spread);
 
-        label_6 = new QLabel(layoutWidget);
+        label_6 = new QLabel(widget);
         label_6->setObjectName(QString::fromUtf8("label_6"));
 
         formLayout->setWidget(5, QFormLayout::FieldRole, label_6);
 
 
-        gridLayout->addLayout(formLayout, 0, 0, 3, 1);
+        horizontalLayout->addLayout(formLayout);
 
-        setup = new QPushButton(layoutWidget);
-        setup->setObjectName(QString::fromUtf8("setup"));
+        horizontalSpacer = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
 
-        gridLayout->addWidget(setup, 0, 2, 1, 1);
+        horizontalLayout->addItem(horizontalSpacer);
 
-        linedown = new QLineEdit(layoutWidget);
-        linedown->setObjectName(QString::fromUtf8("linedown"));
-
-        gridLayout->addWidget(linedown, 1, 1, 1, 1);
-
-        lineup = new QLineEdit(layoutWidget);
+        gridLayout = new QGridLayout();
+        gridLayout->setSpacing(6);
+        gridLayout->setObjectName(QString::fromUtf8("gridLayout"));
+        lineup = new QLineEdit(widget);
         lineup->setObjectName(QString::fromUtf8("lineup"));
 
-        gridLayout->addWidget(lineup, 0, 1, 1, 1);
+        gridLayout->addWidget(lineup, 0, 0, 1, 1);
 
-        label_down = new QLabel(layoutWidget);
-        label_down->setObjectName(QString::fromUtf8("label_down"));
+        setup = new QPushButton(widget);
+        setup->setObjectName(QString::fromUtf8("setup"));
 
-        gridLayout->addWidget(label_down, 1, 3, 1, 1);
+        gridLayout->addWidget(setup, 0, 1, 1, 1);
 
-        label_up = new QLabel(layoutWidget);
+        label_up = new QLabel(widget);
         label_up->setObjectName(QString::fromUtf8("label_up"));
 
-        gridLayout->addWidget(label_up, 0, 3, 1, 1);
+        gridLayout->addWidget(label_up, 0, 2, 1, 1);
+
+        linedown = new QLineEdit(widget);
+        linedown->setObjectName(QString::fromUtf8("linedown"));
+
+        gridLayout->addWidget(linedown, 1, 0, 1, 1);
+
+        setdown = new QPushButton(widget);
+        setdown->setObjectName(QString::fromUtf8("setdown"));
+
+        gridLayout->addWidget(setdown, 1, 1, 1, 1);
+
+        label_down = new QLabel(widget);
+        label_down->setObjectName(QString::fromUtf8("label_down"));
+
+        gridLayout->addWidget(label_down, 1, 2, 1, 1);
+
+        linepath = new QLineEdit(widget);
+        linepath->setObjectName(QString::fromUtf8("linepath"));
+
+        gridLayout->addWidget(linepath, 2, 0, 1, 1);
+
+        setpath = new QPushButton(widget);
+        setpath->setObjectName(QString::fromUtf8("setpath"));
+
+        gridLayout->addWidget(setpath, 2, 1, 1, 1);
+
+
+        horizontalLayout->addLayout(gridLayout);
 
         MainWindow->setCentralWidget(centralWidget);
         menuBar = new QMenuBar(MainWindow);
@@ -211,9 +225,6 @@ public:
     {
         MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", 0, QApplication::UnicodeUTF8));
         actionNew_Ticker->setText(QApplication::translate("MainWindow", "New Ticker", 0, QApplication::UnicodeUTF8));
-        setdown->setText(QApplication::translate("MainWindow", "Set down", 0, QApplication::UnicodeUTF8));
-        setpath->setText(QApplication::translate("MainWindow", "Set Path", 0, QApplication::UnicodeUTF8));
-        linepath->setText(QString());
         price->setText(QApplication::translate("MainWindow", "Price:", 0, QApplication::UnicodeUTF8));
         label->setText(QApplication::translate("MainWindow", "Fetching Data...", 0, QApplication::UnicodeUTF8));
         high->setText(QApplication::translate("MainWindow", "Daily High:", 0, QApplication::UnicodeUTF8));
@@ -226,11 +237,14 @@ public:
         label_5->setText(QApplication::translate("MainWindow", "Fetching Data...", 0, QApplication::UnicodeUTF8));
         spread->setText(QApplication::translate("MainWindow", "Spread", 0, QApplication::UnicodeUTF8));
         label_6->setText(QApplication::translate("MainWindow", "Fetching Data...", 0, QApplication::UnicodeUTF8));
-        setup->setText(QApplication::translate("MainWindow", "Set Up", 0, QApplication::UnicodeUTF8));
-        linedown->setText(QString());
         lineup->setText(QString());
-        label_down->setText(QString());
+        setup->setText(QApplication::translate("MainWindow", "Set Up", 0, QApplication::UnicodeUTF8));
         label_up->setText(QString());
+        linedown->setText(QString());
+        setdown->setText(QApplication::translate("MainWindow", "Set down", 0, QApplication::UnicodeUTF8));
+        label_down->setText(QString());
+        linepath->setText(QString());
+        setpath->setText(QApplication::translate("MainWindow", "Set Path", 0, QApplication::UnicodeUTF8));
         menuFile->setTitle(QApplication::translate("MainWindow", "File", 0, QApplication::UnicodeUTF8));
     } // retranslateUi
 
