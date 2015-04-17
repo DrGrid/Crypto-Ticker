d8a50554271e2c9f96b6f0a96f5993e69dcc5d9a
Just a Charlatan <seb.kung@gmail.com>
2015-04-17 09:29:14 +0200

    Update curl_worker_class.h

--- src/curl_worker_class.h
+++ src/curl_worker_class.h
@@ -5,20 +5,21 @@
 #include "curl_wrapper_class.h"
 #include <string>
 #include <QString>
+#include <thread>
 
 class curl_worker: public QObject
 {
     Q_OBJECT
 private:
-    CppCurl curling;
-    std::string curl_data;
-    QString my_data;
+    CppCurl okcoin_curling, btcchina_curling, bitfinex_curling, bitstamp_curling;
+    std::string okcoin_string, btcchina_string, bitfinex_string, bitstamp_string;
+    QString okcoin_data, btcchina_data. bitfinex_data. bitstamp_data;
 public:
     curl_worker();
 public slots:
     void process();
 signals:
-    void finished_thread (QString my_data);
+    void finished_okcoin (QString okcoin_data);
 };
 
 #endif
