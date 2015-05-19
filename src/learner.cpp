af2daf0ed626c882e7203552aca4828ced678db4
TheCharlatan <seb.kung@gmail.com>
2015-05-19 23:00:42 +0200

    Add first rules to learner class

--- src/learner.cpp
+++ src/learner.cpp
@@ -3,17 +3,45 @@
 Learner::Learner(QObject *parent) :
     QObject(parent)
 {
+    china_move = false;
+    usd_move = false;
 }
 
 void Learner::data_feeder(double china1_current, double china2_current, double usd1_current, double usd2_current)
 {
-    china1 = push_vector(china1_current, china1);
-    china2 = push_vector(china2_current, china2);
-    usd1 = push_vector(usd1_current, usd1);
-    usd2 = push_vector(usd2_current, usd2);
+    if ((china1_current-china2_current > 0.1) | (china1_current-china2_current < -0.1))
+        china_move = true;
+    if ((china1.size() == 11) && (zero_movement(china1,china2)))
+        china_move = false;
+    if (china_move)
+    {
+        china1 = push_vector(china1_current, china1);
+        china2 = push_vector(china2_current, china2);
+    }
+    if ((usd1_current-usd2_current > 0.01) | (usd1_current-usd2_current < -0.01))
+        usd_move = true;
+    if ((usd1.size() == 11) && (zero_movement(usd1,usd2)))
+        usd_move = false;
+    if (usd_move)
+    {
+        usd1 = push_vector(usd1_current, usd1);
+        usd2 = push_vector(usd2_current, usd2);
+    }
 }
 
-QVector<double> Learner::push_vector(double current, QVector<double>& new_storage)
+bool Learner::zero_movement(QVector<double> &once, QVector<double> &twice)
+{
+    if (once.size() == 11)
+        return false;
+    for (unsigned short c(0); c <= 10; c++)
+    {
+        if ((once[c]-twice[c] > 0.1) | (once[c]-twice[c] < -0.1))
+            return false;
+    }
+    return true;
+}
+
+QVector<double> Learner::push_vector(double current, QVector<double> &new_storage)
 {
     if (new_storage.size() <= 12)
         new_storage.push_back(current);
--- src/learner.h
+++ src/learner.h
@@ -13,6 +13,7 @@ public:
     double delta(QVector<double> &marketlist, int difference);
     bool significant_discrimination(double market_1, double market_2);
     void predictor();
+    bool zero_movement(QVector<double> &once, QVector<double> &twice);
 signals:
 
 public slots:
@@ -23,6 +24,7 @@ private:
     double okcoin_delta_five, okcoin_delta_ten, btcchina_delta_five, btcchina_delta_ten;
     QVector<double> china1, china2, usd1, usd2;
     QVector<double> btcchina_history, okcoin_history;
+    bool china_move, usd_move;
 };
 
 #endif // LEARNER_H
