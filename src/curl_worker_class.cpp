5563916dc6857bbb7c5519ba10355b7e571d3325
TheCharlatan <seb.kung@gmail.com>
2015-06-02 23:43:25 +0200

    Add control of OkCoin Request numbers

--- src/curl_worker_class.cpp
+++ src/curl_worker_class.cpp
@@ -1,6 +1,7 @@
 #include "curl_worker_class.h"
 #include <thread>
-
+#include <chrono>
+#include <ctime>
 // --- PROCESS ---
 // Start processing data.
 
@@ -23,8 +24,21 @@ void curl_worker::process()
 
 void curl_worker::okcoin_process()
 {
+    unsigned short nrequests; //use this varible to control the number of requests the user is making, it has to be sub 40, otherwise he will be banned from the API.
+    std::time_t unix_time = std::time(nullptr);
+    long prevtime;
     while (true)
     {
+        prevtime = unix_time;
+        unix_time = std::time(nullptr);
+        if (prevtime == unix_time)
+        {
+          nrequests++;
+          if (nrequests == 20)
+              std::this_thread::sleep_for(std::chrono::seconds(1)); //wait a bit, in case the client queries the api too often.
+        }
+        else
+          nrequests = 0;
         okcoin_string = okcoin_curling.fetch();
         okcoin_data = QString::fromStdString(okcoin_string);
         okcoin_string.clear();
